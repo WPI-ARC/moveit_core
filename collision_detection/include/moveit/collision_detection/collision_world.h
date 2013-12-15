17ce58acf2c9ba39d0658e2d62f88781ec0d86e9
Quan Peng <quan1992@wpi.edu>
2013-12-15 16:35:24 -0500

    online HRSS

--- collision_detection/include/moveit/collision_detection/collision_world.h
+++ collision_detection/include/moveit/collision_detection/collision_world.h
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of Willow Garage nor the names of its
+ *   * Neither the name of the Willow Garage nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -215,7 +215,7 @@ namespace collision_detection
     virtual double distanceWorld(const CollisionWorld &world,
                                  const AllowedCollisionMatrix &acm) const = 0;
     /** set the world to use.
-     * This can be expensive unless the new and old world are empty.
+     * This can be expensive unless the new and old world are empty. 
      * Passing NULL will result in a new empty world being created. */
     virtual void setWorld(const WorldPtr& world);
 
